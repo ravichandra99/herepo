445a050a5019cdaa3576f2db296c775ee6a01853
Ravi Chandra <ravichandra178@gmail.com>
2021-02-27 10:28:20 +0000

    lbt2

--- yaksh_data/yaksh/static/yaksh/js/findreplace.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $("body").children().each(function () {
-        $(this).html( $(this).html().replace("Summary","Question") );
-        $(this).html( $(this).html().replace("Points","Marks") );
-    });
-});
